42dacd747ef3bffa599095a344663e3fd2d3430a
itshmnt <hemantxs5@gmail.com>
2021-01-16 23:25:06 +0530

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
 class DishDetail extends Component {
 	render() {
 		const dish = this.props.selectedDish
-		if(dish == null || dish == undefined) return (<div></div>)
+		if(dish === null || dish === undefined) return (<div></div>)
 
 		return (
-			<div>
+			<div class="container">
 				<div className = 'row'>
 					<div className = 'col-12 col-md-5 m-1'>
 						<Card>
@@ -24,7 +24,7 @@ class DishDetail extends Component {
 							return (
 								<div key = {comment.id}>
 									<p>{comment.comment}</p>
-									<p>{`-- ${comment.author} , ${comment.date}`}</p>
+									<p>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}</p>
 								</div>
 							)
 						})}			
