3805b9ddc715f76c49dcd03109b26d34a65e76f9
itshmnt <hemantxs5@gmail.com>
2021-03-31 09:58:50 +0530

    Review Form

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,3 +1,110 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText } from "reactstrap";
+import { Link } from 'react-router-dom';
+import {
+    Button, Modal, ModalBody, ModalHeader, Label, Row, Col
+} from "reactstrap";
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import CommentForm from "./CommentFormComponent";
+
+
+function RenderDish({dish}) {
+    if (dish != null) {
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle> {dish.name}</CardTitle>
+                        <CardText> {dish.description} </CardText>
+                    </CardBody>
+                </Card>
+            </div>   
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+function RenderComments({dish,comments}){
+    if (comments == null) {
+        return (<div></div>)
+    }
+    const cmnts = comments.map(comment => {
+        return (
+
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author},
+                &nbsp;
+                {new Intl.DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: '2-digit'
+                }).format(new Date(comment.date))}
+                </p>
+            </li>
+
+        )
+    })
+    return (
+        <div className='col-12 col-md-5 m-1'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {cmnts}
+            </ul>
+            <CommentForm dish={dish} comments={comments} />
+        </div>
+    )
+}
+
+
+const DishDetail = (props) => {
+
+    const dish = props.dish
+
+
+    if (dish == null) {
+        return (<div></div>);
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to="/menu">Menu</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        { props.dish.name }
+                    </BreadcrumbItem>
+                </Breadcrumb>
+
+                <div className="col-12">
+                    <h3> {props.dish.menu}</h3>
+                    <hr />
+                </div>
+            </div>
+
+            <div className='row'>
+                <RenderDish dish={ props.dish } />
+                <RenderComments dish={props.dish} comments={ props.comments } />
+            </div>
+
+
+        </div>
+    )
+}
+
+
+
+export default DishDetail; 
+
+/*
+------------------------------BEFORE FEEDBACK FORM-------------------------------
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
@@ -79,3 +186,5 @@ const DishDetail = (props) => {
 };
 
 export default DishDetail;
+
+*/
